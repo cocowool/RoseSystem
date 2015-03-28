bc6cfa97a0bf89f60071ef2f8bb08e0c996cf72e
cocowool <cocowool@gmail.com>
2015-03-28 19:00:04 +0800

    修改首页DOM结构

--- application/views/index.php
+++ application/views/index.php
@@ -78,9 +78,15 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4"></div>
-			<div class="col-md-4"></div>
-			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				
+			</div>
+			<div class="col-md-4">
+			
+			</div>
+			<div class="col-md-4">
+			
+			</div>
 		</div>
 	</div>
 	<div class="container ys_footer">
