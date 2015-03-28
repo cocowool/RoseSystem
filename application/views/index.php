fa89f8c7eb1e33b9f414516f9dfc1c7ab3d6b096
cocowool <cocowool@gmail.com>
2015-03-28 18:11:09 +0800

    修改样式表，切图首页

--- application/views/index.php
+++ application/views/index.php
@@ -1,3 +1,108 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<title></title>
+	<meta name="keyword" content="">
+	<meta name="description" content="">
+	<link rel="icon" href="/favicon.ico">
+	<link href="/libs/bootstrap-3.3.4/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="/libs/bootstrap-3.3.4/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+	<link href="/templates/yueshi/css/main.css" rel="stylesheet" media="screen">
+	<!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+	<nav class="navbar ys_header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 ys_search">
+					<p><input type='text' name='ys_site_search' id='ys_site_search' /><a href="#">搜索</a></p>
+				</div>
+				<div class="col-md-8">
+					<p>
+						<a href="javascript:void(0);">注册</a>
+						<input type='text' name='ys_username' id='ys_username' />
+						<input type='text' name='ys_password' id='ys_password' />
+						<a href="javascript:void(0);">登录</a>
+						<a href="javascript:void(0);">忘记密码</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10">
+				<div class='ys_top_title'>
+					<h1><a href="javascript:void(0);">悦食中国</a></h1>
+				</div>
+				<div id="carousel-example-generic" class="carousel slide ys_homefocus" data-ride="carousel">
+			        <ol class="carousel-indicators">
+         	 			<li data-target="#carousel-example-generic" data-slide-to="0" class=""></li>
+				        <li data-target="#carousel-example-generic" data-slide-to="1" class="active"></li>
+				        <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+			        </ol>
+			        <div class="carousel-inner" role="listbox">
+          				<div class="item">
+            				<img data-src="holder.js/1140x500/auto/#777:#555/text:First slide" alt="First slide [1140x500]" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTE0MCIgaGVpZ2h0PSI1MDAiIHZpZXdCb3g9IjAgMCAxMTQwIDUwMCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSIxMTQwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyIvPjxnPjx0ZXh0IHg9IjQwMi4zMDQ2ODc1IiB5PSIyNTAiIHN0eWxlPSJmaWxsOiM1NTU7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NTNwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj5GaXJzdCBzbGlkZTwvdGV4dD48L2c+PC9zdmc+" data-holder-rendered="true">
+          				</div>
+          				<div class="item active">
+            				<img data-src="holder.js/1140x500/auto/#666:#444/text:Second slide" alt="Second slide [1140x500]" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTE0MCIgaGVpZ2h0PSI1MDAiIHZpZXdCb3g9IjAgMCAxMTQwIDUwMCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSIxMTQwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiIvPjxnPjx0ZXh0IHg9IjM1MS4wMTU2MjUiIHk9IjI1MCIgc3R5bGU9ImZpbGw6IzQ0NDtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZTo1M3B0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPlNlY29uZCBzbGlkZTwvdGV4dD48L2c+PC9zdmc+" data-holder-rendered="true">
+          				</div>
+	          			<div class="item">
+            				<img data-src="holder.js/1140x500/auto/#555:#333/text:Third slide" alt="Third slide [1140x500]" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTE0MCIgaGVpZ2h0PSI1MDAiIHZpZXdCb3g9IjAgMCAxMTQwIDUwMCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSIxMTQwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzU1NSIvPjxnPjx0ZXh0IHg9IjM5MC41IiB5PSIyNTAiIHN0eWxlPSJmaWxsOiMzMzM7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NTNwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj5UaGlyZCBzbGlkZTwvdGV4dD48L2c+PC9zdmc+" data-holder-rendered="true">
+          				</div>
+        			</div>
+        			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          				<span class="sr-only">Previous</span>
+        			</a>
+        			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          				<span class="sr-only">Next</span>
+        			</a>
+      			</div>
+			</div>
+			<div class="col-md-2">
+				<div class="ys_logo">
+					<h2><a href="javascript:void(0);">悦食中国</a></h2>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+			<div class="col-md-4"></div>
+		</div>
+	</div>
+	<div class="container ys_footer">
+		<div class="row">
+			<div class="col-md-5">
+				CopyRight&copy;2015 Vita Group(China). All Rights Reserved.
+			</div>
+			<div class="col-md-7">
+				<ul class="ys_foot_links">
+					<li><a href="/article/detail/18">©官方微信</a></li>
+					<li><a href="http://e.weibo.com/yszg2012/profile">©官方微博</a></li>
+					<li><a href="/article/detail/48">©联系我们</a></li>
+					<li><a href="/article/detail/12">©用户服务条款</a></li>
+					<li><a href="/article/detail/11">©隐私权声明</a></li>
+					<li><a href="/register">注册登录</a></li>
+					<li><a href="/events">会员活动</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 
-echo "Hello World!<br />";
+	<script type="text/javascript" src="/libs/jquery/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="/libs/bootstrap-3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
