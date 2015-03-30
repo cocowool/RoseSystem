5b868297b6835f4ea258a2004f7ad5fd4116c447
cocowool <cocowool@gmail.com>
2015-03-30 21:25:14 +0800

    增加文章和列表控制器

---
+++ application/controllers/article.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Article extends My_Controller {
+
+	public function __construct(){
+		parent::__construct();
+	}
+	
+}
