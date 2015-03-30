5d69e8c5b4b2de836ead91a47ce106d5b5ecabb1
Green Wang <cocowool@gmail.com>
2015-03-30 13:13:35 +0800

    修改路由器配置，支持后台管理路径

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,8 @@
 $route['default_controller'] = "index";
 $route['404_override'] = '';
 
+$route['manage'] = '/manage/index';
+
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
