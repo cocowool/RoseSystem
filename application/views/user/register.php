30030c0b8b8e8b851e041ebe2eb489ddadcbd393
cocowool <cocowool@gmail.com>
2015-04-01 21:40:22 +0800

    [前台]调试用户注册页面样式

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -43,121 +43,89 @@
 				</div>
 				
 				<div class="ys_container_register">
-				<form name="registerForm" id="registerForm" action="/user/register" method="post">
 				<?php if(validation_errors() ){ ?>
-				<div class="register-errors">
+				<div class="ys_tips_error">
 					<?php echo validation_errors(); ?>
 				</div>
 				<?php } ?>
-				<h2 class="register-title">会员注册</h2>
-				<div class="register-section">
-					<div class="register-item">
-						<span class="block-left register-label">用户名称<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-username">
-								<input type="text" name="username" id="username" placeholder="请输入用户名" value="<?php echo set_value('username'); ?>" />
-							</div>
-						</div>
+				<h2 class="ys_title_register">会员注册</h2>
+				<form name="registerForm" id="registerForm" action="/user/register" method="post" class="">
+				<div class="ys_form_section">
+					<div class="form-group">
+						<label class="ys_form_label" for="username">用户名称<b>*</b></label>
+						<input type="text" class="form-control" name="username" id="username" placeholder="请输入用户名" value="<?php echo set_value('username'); ?>" />
 					</div>
-					<div class="register-item">
-						<span class="block-left register-label">用户密码<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-username">
-								<input type="password" name="password" id="password" value="<?php echo set_value('password'); ?>" />
-								<label class="focus two-line hide">6-20位字符，可使用数字、字母和符号的组合</label>
-							</div>
-						</div>
+					<div class="form-group">
+						<label for="password" class="ys_form_label">用户密码<b>*</b></label>
+						<input type="password" class="form-control" name="password" id="password" value="<?php echo set_value('password'); ?>" />
+						<span class="focus two-line hide">6-20位字符，可使用数字、字母和符号的组合</span>
 					</div>
-					<div class="register-item">
-						<span class="block-left register-label">确认密码<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-username">
-								<input type="password" name="chkPassword" id="chkPassword" value="<?php echo set_value('chkPassword'); ?>" />
-								<label class="focus hide">请再次输入密码</label>
-							</div>
-						</div>
+					<div class="form-group">
+						<label class="ys_form_label" for="chkPassword">确认密码<b>*</b></label>
+						<input type="password" class="form-control" name="chkPassword" id="chkPassword" value="<?php echo set_value('chkPassword'); ?>" />
+						<span class="focus hide">请再次输入密码</span>
 					</div>
-					<div class="register-item">
-						<span class="block-left register-label">姓<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-username">
-								<input type="text" name="lastname" id="lastname" value="<?php echo set_value('lastname'); ?>" />
-								<label class="focus hide">请输入姓氏</label>
-							</div>
-						</div>
+					<div class="form-group">
+						<label class="ys_form_label" for="lastname">姓<b>*</b></label>
+						<input type="text" class="form-control" name="lastname" id="lastname" value="<?php echo set_value('lastname'); ?>" />
+						<span class="focus hide">请输入姓氏</span>
 					</div>
-					<div class="register-item">
-						<span class="block-left register-label">名<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-username">
-								<input type="text" name="firstname" id="firstname" value="<?php echo set_value('firstname'); ?>" />
-								<label class="focus hide">请输入您的名字</label>
-							</div>
-						</div>
+					<div class="form-group">
+						<label class="ys_form_label" for="firstname">名<b>*</b></label>
+						<input type="text" class="form-control" name="firstname" id="firstname" value="<?php echo set_value('firstname'); ?>" />
+						<span class="focus hide">请输入您的名字</span>
 					</div>
-					<div class="register-item">
-						<span class="block-left register-label">性别<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-gender">
-								<input type="radio" name="gender" value="1" <?php echo set_radio('gender', '1', true); ?> />&nbsp;男&nbsp;
-								<input type="radio" name="gender" value="0" <?php echo set_radio('gender', '0'); ?> />&nbsp;女
-							</div>
-						</div>
+					<div class="form-group">
+						<label class="ys_form_label" for="gender">性别<b>*</b></label>
+						<input type="radio" class="form-control" name="gender" value="1" <?php echo set_radio('gender', '1', true); ?> />&nbsp;男&nbsp;
+						<input type="radio" class="form-control" name="gender" value="0" <?php echo set_radio('gender', '0'); ?> />&nbsp;女
 					</div>
-					<div class="register-item">
-						<span class="block-left register-label">生日<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-username">
-								<input type="text" name="birthday" id="birthday" value="<?php echo set_value('birthday'); ?>" />
-								<label class="focus hide">请选择您的出生日期</label>
-							</div>
-						</div>
+					<div class="form-group">
+						<label class="ys_form_label" for="birthday">生日<b>*</b></label>
+						<input type="text" class="form-control" name="birthday" id="birthday" value="<?php echo set_value('birthday'); ?>" />
+						<label class="focus hide">请选择您的出生日期</label>
 					</div>
-					<div class="register-item">
-						<span class="block-left register-label">居住地<b>*</b></span>
-						<div class="block-right register-content input-item">
-							<div class="user-input register-username">
-								<select id="hometown" name="hometown" class="form-select">
-		                            <option value="">请选择</option>
-						            <option value="北京">北京</option>
-						            <option value="上海">上海</option>
-						            <option value="天津">天津</option>
-						            <option value="重庆">重庆</option>
-						            <option value="河北">河北</option>
-						            <option value="山西">山西</option>
-						            <option value="河南">河南</option>
-						            <option value="辽宁">辽宁</option>
-						            <option value="吉林">吉林</option>
-						            <option value="黑龙江">黑龙江</option>
-						            <option value="内蒙古">内蒙古</option>
-						            <option value="江苏">江苏</option>
-						            <option value="山东">山东</option>
-						            <option value="安徽">安徽</option>
-						            <option value="浙江">浙江</option>
-						            <option value="福建">福建</option>
-						            <option value="湖北">湖北</option>
-						            <option value="湖南">湖南</option>
-						            <option value="广东">广东</option>
-						            <option value="广西">广西</option>
-						            <option value="江西">江西</option>
-						            <option value="四川">四川</option>
-						            <option value="海南">海南</option>
-						            <option value="贵州">贵州</option>
-						            <option value="云南">云南</option>
-						            <option value="西藏">西藏</option>
-						            <option value="陕西">陕西</option>
-						            <option value="甘肃">甘肃</option>
-						            <option value="青海">青海</option>
-						            <option value="宁夏">宁夏</option>
-						            <option value="新疆">新疆</option>
-						            <option value="台湾">台湾</option>
-						            <option value="香港">香港</option>
-						            <option value="澳门">澳门</option>
-						            <option value="海外">海外</option>
-					            </select>
-								<label class="focus hide">请选择居住地</label>
-							</div>
-						</div>
+					<div class="form-group">
+						<label class="ys_form_label" for="hometown">居住地<b>*</b></label>
+						<select id="hometown" name="hometown" class="form-control">
+                            <option value="">请选择</option>
+				            <option value="北京">北京</option>
+				            <option value="上海">上海</option>
+				            <option value="天津">天津</option>
+				            <option value="重庆">重庆</option>
+				            <option value="河北">河北</option>
+				            <option value="山西">山西</option>
+				            <option value="河南">河南</option>
+				            <option value="辽宁">辽宁</option>
+				            <option value="吉林">吉林</option>
+				            <option value="黑龙江">黑龙江</option>
+				            <option value="内蒙古">内蒙古</option>
+				            <option value="江苏">江苏</option>
+				            <option value="山东">山东</option>
+				            <option value="安徽">安徽</option>
+				            <option value="浙江">浙江</option>
+				            <option value="福建">福建</option>
+				            <option value="湖北">湖北</option>
+				            <option value="湖南">湖南</option>
+				            <option value="广东">广东</option>
+				            <option value="广西">广西</option>
+				            <option value="江西">江西</option>
+				            <option value="四川">四川</option>
+				            <option value="海南">海南</option>
+				            <option value="贵州">贵州</option>
+				            <option value="云南">云南</option>
+				            <option value="西藏">西藏</option>
+				            <option value="陕西">陕西</option>
+				            <option value="甘肃">甘肃</option>
+				            <option value="青海">青海</option>
+				            <option value="宁夏">宁夏</option>
+				            <option value="新疆">新疆</option>
+				            <option value="台湾">台湾</option>
+				            <option value="香港">香港</option>
+				            <option value="澳门">澳门</option>
+				            <option value="海外">海外</option>
+			            </select>
+						<label class="focus hide">请选择居住地</label>
 					</div>
 					<div class="register-item">
 						<span class="block-left register-label">电子邮箱<b>*</b></span>
