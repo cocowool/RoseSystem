d43b2825f19fcefdbaed586f4c84c59d67b4663f
Green Wang <cocowool@gmail.com>
2015-04-07 12:51:57 +0800

    [后台]完善后台视频管理

--- application/models/video_model.php
+++ application/models/video_model.php
@@ -6,8 +6,12 @@ class Video_Model extends MY_Model {
 	protected $fields = array(
 			array('name'=>'id', 'comment'=>'序号', 'primary' => TRUE),
 			array('name'=>'v_title', 'comment'=>'视频名称'),
+			array('name'=>'v_category', 'comment'=>'视频分类'),
 			array('name'=>'v_desc', 'comment'=>'视频简介'),
+			array('name'=>'v_thumb', 'comment'=>'视频缩略图'),
 			array('name'=>'v_click', 'comment'=>'访问次数'),
+			array('name'=>'v_like', 'comment'=>'喜欢次数'),
+			array('name'=>'v_fav', 'comment'=>'收藏次数'),
 			array('name'=>'v_youku', 'comment'=>'优酷链接'),
 			array('name'=>'status', 'comment'=>'状态', 'invisible' => TRUE),
 			array('name'=>'insert_time', 'comment'=>'创建时间'),
--- application/views/manage/video/video_list.php
+++ application/views/manage/video/video_list.php
@@ -1,9 +1,25 @@
 <h1>视频管理</h1>
 <div class="rs_submenu">
-	<p>
-		<a href="javascript:void(0);">视频分类</a>&nbsp;&nbsp;
-		<a href="javascript:void(0);">视频列表</a>&nbsp;&nbsp;
-	</p>
+	<div class="dropdown">
+		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+    		视频分类
+    		<span class="caret"></span>
+  		</button>
+		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">分类列表</a></li>
+		    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">新增分类</a></li>
+		</ul>
+	</div>
+	<div class="dropdown">
+		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+    		视频列表
+    		<span class="caret"></span>
+  		</button>
+		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">视频列表</a></li>
+		    <li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/video/add">新增视频</a></li>
+		</ul>
+	</div>
 </div>
 <div class="rs_container">
 	<div class="rs_container_condition">
