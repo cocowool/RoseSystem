2d8eceee0111000971e3a693f1ef2926e928b61d
cocowool <cocowool@gmail.com>
2015-04-09 21:31:34 +0800

    [后台]添加必填项规则

--- application/models/video_model.php
+++ application/models/video_model.php
@@ -5,7 +5,7 @@ class Video_Model extends MY_Model {
 	public $id	=	'id';
 	public $fields = array(
 			array('name'=>'id', 'comment'=>'序号', 'primary' => TRUE, 'form'=>array('type'=>'primary')),
-			array('name'=>'v_title', 'comment'=>'视频名称', 'form'=> array('type'=>'text'), 'validation'=>array('required') ),
+			array('name'=>'v_title', 'comment'=>'视频名称', 'form'=> array('type'=>'text', 'validation'=>'required', 'tips'=>'请输入视频的名称')),
 			array('name'=>'v_category', 'comment'=>'视频分类'),
 			array('name'=>'v_desc', 'comment'=>'视频简介', 'form'=> array('type'=>'textarea')),
 			array('name'=>'v_thumb', 'comment'=>'视频缩略图', 'form'=> array('type'=>'text')),
