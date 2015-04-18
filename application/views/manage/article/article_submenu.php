ab23b31ae375dd5a66caacea7431e059ed75f3f2
cocowool <cocowool@gmail.com>
2015-04-18 22:58:53 +0800

    [后台]增加图片资源页面

--- application/views/manage/article/article_submenu.php
+++ application/views/manage/article/article_submenu.php
@@ -22,4 +22,14 @@
 		    <li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/article/add">新增文章</a></li>
 		</ul>
 	</div>
+	<div class="dropdown">
+		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+    		图片资源
+    		<span class="caret"></span>
+  		</button>
+		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/resource">图片列表</a></li>
+		    <li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/resource/add">新增图片</a></li>
+		</ul>
+	</div>
 </div>
