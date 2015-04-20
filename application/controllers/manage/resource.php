4762613665ff19b5c4d0d77644adcac7c553d2f0
Green Wang <cocowool@gmail.com>
2015-04-20 11:30:34 +0800

    [后台]完善图片资源修改

--- application/controllers/manage/resource.php
+++ application/controllers/manage/resource.php
@@ -26,11 +26,28 @@ class Resource extends MY_Controller {
 		//可以在此处进行返回数据的自定义处理
 		foreach($data['data'] as $k=>$v){
 			$data['data'][$k]['image'] = '<a href=""><img src="' .$v['web_path'] .'" height="50px" /></a>';
-			$data['data'][$k]['operation'] = '<a href="/manage/article/edit/' . $v['id'] . '">编辑</a>&nbsp;&nbsp;';
-			$data['data'][$k]['operation'] .= '<a href="/manage/article/del/' . $v['id'] . '">删除</a>';
+			$data['data'][$k]['operation'] = '<a href="/manage/resource/edit/' . $v['id'] . '">编辑</a>&nbsp;&nbsp;';
+			$data['data'][$k]['operation'] .= '<a href="/manage/resource/del/' . $v['id'] . '">删除</a>';
 		}
 		
 		echo json_encode($data);
+	}
+	
+	/**
+	 * 删除图片资源
+	 * 
+	 * @param int $id
+	 */
+	public function del($id){
+		$data = array();
+		$this->load->model('Resource_Model','r');
+		if(empty($id) && !$this->r->getById($id) ){
+			$this->redirectAction(FALSE, $data, '', '/manage/article');
+		}
+		
+		$result = $this->r->delete($id);
+		$this->redirectAction($result, $data, '', '/manage/article');
+		$this->load->view('manage/main', $data);
 	}
 	
 	public function add(){
@@ -73,7 +90,7 @@ class Resource extends MY_Controller {
 	public function edit($id){
 		$data = array();
 	
-		$this->load->model('Article_Model','a');
+		$this->load->model('Resource_Model','r');
 		$this->lang->load('form_validation', 'chinese');
 		$validations = array(
 				array(
@@ -92,18 +109,18 @@ class Resource extends MY_Controller {
 		$this->form_validation->set_error_delimiters('<span class="help-block">', '</span>');
 	
 		if($this->form_validation->run() == FALSE){
-			$data = $this->a->getById($id);
-			$data['html_form'] = $this->generate_edit_form($data, $this->a, 'manage/article/edit/' . $id);
+			$data = $this->r->getById($id);
+			$data['html_form'] = $this->generate_edit_form($data, $this->r, 'manage/resource/edit/' . $id);
 	
-			$data['content_view'] = 'manage/article/article_add';
+			$data['content_view'] = 'manage/resource/resource_add';
 			$data['content_data'] = '';
 			$this->load->view('manage/main', $data);
 		}else{
 			$this->load->helper('date');
 			$data = $this->input->post(NULL, true);
 	
-			$result = $this->a->update( $data, $id );
-			$this->redirectAction($result, $data, '/manage/article', '/manage/article/edit'.$id);
+			$result = $this->r->update( $data, $id );
+			$this->redirectAction($result, $data, '/manage/resource/' . $id, '/manage/resource/edit'.$id);
 		}
 	}
 }
