4c85ccef0f5b73106e78274893517200185bd6ee
cocowool <cocowool@gmail.com>
2015-04-22 21:55:21 +0800

    [后台]初步编辑chef页面

--- application/controllers/manage/chef.php
+++ application/controllers/manage/chef.php
@@ -1,1 +1,13 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Chef extends MY_Controller {
+	private $colum_title = '厨艺学校管理';
+
+	public function __construct(){
+		parent::__construct();
+	}
+	
+	public function index(){
+		
+	}
+}
