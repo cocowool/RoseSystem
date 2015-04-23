94e1dbce18a18df70909282ade5ac75b7da70cd6
Green Wang <cocowool@gmail.com>
2015-04-23 15:43:58 +0800

    [后台]完善评论查看页

--- application/models/comment_model.php
+++ application/models/comment_model.php
@@ -1,1 +1,28 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Comment_Model extends MY_Model {
+	public $table = 'r_comment';
+	public $id	=	'id';
+	public $fields = array(
+			array('name'=>'id', 'comment'=>'序号', 'primary' => TRUE, 'form'=>array('type'=>'primary')),
+			array('name'=>'ctype', 'comment'=>'评论类型', 'form'=>array('type'=>'select', 
+				'option'=>array('type'=>'static', 'data'=>array('1'=>'杂志','2'=>'影像')))
+			),
+			array('name'=>'cid', 'comment'=>'资源ID', 'form'=>array('type'=>'text')),
+			array('name'=>'userid', 'comment'=>'用户ID', 'form'=> array('type'=>'text', 'validation'=>'required', 'tips'=>'请输入商品的名称')),
+			array('name'=>'content', 'comment'=>'评论内容', 'form'=> array('type'=>'textarea', 'validation'=>'required', 'tips'=>'请输入商品的简介')),
+			array('name'=>'userip', 'comment'=>'用户IP', 'form'=> array('type'=>'textarea', 'validation'=>'required', 'tips'=>'请输入商品的简介')),
+			array('name'=>'ua', 'comment'=>'UserAgent', 'form'=> array('type'=>'file', 'validation'=>'required', 'edit'=>'img') ),	//存储WEB访问调用地址
+			array('name'=>'status', 'comment'=>'状态', 'form'=>array('type'=>'select', 
+				'option'=>array('type'=>'static', 'data'=>array('0'=>'草稿','1'=>'已删除')))
+			),
+			array('name'=>'insert_time', 'comment'=>'创建时间'),
+			array('name'=>'update_time', 'comment'=>'更新时间'),
+			array('name'=>'operation', 'comment'=>'操作'),
+	);
+
+	function __construct(){
+		parent::__construct();
+	}
+
+}
