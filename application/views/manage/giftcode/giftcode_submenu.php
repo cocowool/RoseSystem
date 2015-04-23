8e49234f5e90f9edbdccf0f5bf32b48800588cdb
Green Wang <cocowool@gmail.com>
2015-04-23 15:11:17 +0800

    [后台]补充评论管理相关页面

--- application/views/manage/giftcode/giftcode_submenu.php
+++ application/views/manage/giftcode/giftcode_submenu.php
@@ -1,1 +1,25 @@
-<?php
+<div class="rs_submenu">
+	<div class="dropdown">
+		<span class="h3 rs_menu_title">礼券管理</span>
+	</div>
+	<div class="dropdown">
+		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+    		礼券列表
+    		<span class="caret"></span>
+  		</button>
+		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/giftcode">礼券列表</a></li>
+		    <li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/giftcode/generate">批量生成</a></li>
+		</ul>
+	</div>
+	<div class="dropdown">
+		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+    		订单列表
+    		<span class="caret"></span>
+  		</button>
+		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/giftcode/order">订单列表</a></li>
+		    <li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/giftcode/orderexport">订单导出</a></li>
+		</ul>
+	</div>
+</div>
