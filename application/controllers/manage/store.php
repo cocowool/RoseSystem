11a979b015d0a01885bd995f5106e63cbcfdca62
Green Wang <cocowool@gmail.com>
2015-04-23 10:35:28 +0800

    [后台]完成商品的添加和列表显示

--- application/controllers/manage/store.php
+++ application/controllers/manage/store.php
@@ -18,17 +18,12 @@ class Store extends MY_Controller {
 	 */
 	public function serverside(){
 		$request = $this->input->post();
-		$this->load->model('resource_model','r');
-		
-		//处理自定义条件
-		if( isset($request['aid']) ){
-			$request['condition'][] = array('data'	=>	$request['aid'], 'field' => 'aid', 'action' => 'where'	);
-		}
-		
-		$data = $this->r->dtRequest($request);
+		$this->load->model('store_model','s');
+				
+		$data = $this->s->dtRequest($request);
 		//可以在此处进行返回数据的自定义处理
 		foreach($data['data'] as $k=>$v){
-			$data['data'][$k]['image'] = '<a href=""><img src="' .$v['web_path'] .'" height="50px" /></a>';
+			$data['data'][$k]['s_thumb'] = '<a href=""><img src="' .$v['s_thumb'] .'" height="50px" /></a>';
 			$data['data'][$k]['operation'] = '<a href="/manage/resource/edit/' . $v['id'] . '">编辑</a>&nbsp;&nbsp;';
 			$data['data'][$k]['operation'] .= '<a href="/manage/resource/del/' . $v['id'] . '">删除</a>&nbsp;&nbsp;';
 			$data['data'][$k]['operation'] .= '<a href="javascript:void(0);">复制链接</a>';
@@ -36,4 +31,61 @@ class Store extends MY_Controller {
 		
 		echo json_encode($data);
 	}
+	
+	public function add(){
+		$data = array();
+	
+		$this->load->model('Store_Model','s');
+		$this->lang->load('form_validation', 'chinese');
+		$validations = array(
+				array(
+					'field'	=>	's_title',
+					'label'	=>	'商品名称',
+					'rules'	=>	'trim|required'
+				),
+				array(
+					'field'	=>	's_desc',
+					'label'	=>	'商品简介',
+					'rules'	=>	'trim|required'
+				),
+				array(
+					'field'	=>	's_text',
+					'label'	=>	'商品详情',
+					'rules'	=>	'trim|required'
+				)
+		);
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules($validations);
+		$this->form_validation->set_error_delimiters('<span class="help-block">', '</span>');
+	
+		if($this->form_validation->run() == FALSE){
+			$data['html_form'] = $this->generate_add_form($this->s, 'manage/store/add');
+			$data['content_view'] = 'manage/store/store_add';
+			$data['content_data'] = '';
+			$this->load->view('manage/main', $data);
+		}else{
+			$this->load->helper('date');
+			$_POST['insert_time'] = unix_to_human( local_to_gmt(), TRUE, 'eu');
+			$data = $this->input->post(NULL, true);
+				
+			$config = $this->config->item('image_upload_config');
+			$this->load->library('upload', $config);
+				
+			//if ( ! $this->upload->sae_upload( $this->sae_domain, 'path')){
+			if ( ! $this->upload->do_upload( 's_thumb' ) ){
+				$error = array('error' => $this->upload->display_errors());
+				$data['content_data']['user_text'] = $error['error'];
+				$this->redirectAction(FALSE, $data, '/manage/store', '/manage/store');
+				return false;
+			}else{
+				$updata = array('upload_data' => $this->upload->data());
+				//$data['v_thumb'] = $updata['upload_data']['sae_full_path'];
+				$data['s_thumb'] = 'http://' . $_SERVER['SERVER_NAME'] . '/temp/' . $updata['upload_data']['file_name'];
+				$data['s_location'] = $updata['upload_data']['full_path'];
+			}
+				
+			$result = $this->s->insert( $data );
+			$this->redirectAction($result, $data, '/manage/store', '/manage/store/add');
+		}
+	}	
 }
--- application/views/manage/store/store_submenu.php
+++ application/views/manage/store/store_submenu.php
@@ -1,1 +1,15 @@
-<?php
+<div class="rs_submenu">
+	<div class="dropdown">
+		<span class="h3 rs_menu_title">店铺管理</span>
+	</div>
+	<div class="dropdown">
+		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+    		商品列表
+    		<span class="caret"></span>
+  		</button>
+		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+			<li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/store">商品列表</a></li>
+		    <li role="presentation"><a role="menuitem" tabindex="-1" href="/manage/store/add">新增商品</a></li>
+		</ul>
+	</div>
+</div>
