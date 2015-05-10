d8acc27e9adb550522ddd81a96e97b0d1af3ff3a
cocowool <cocowool@gmail.com>
2015-05-10 14:47:15 +0800

    [后台]完善CHEF的模型

--- application/controllers/chef.php
+++ application/controllers/chef.php
@@ -1,1 +1,13 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Chef extends My_Controller {
+
+	public function __construct(){
+		parent::__construct();
+	}
+	
+	public function index($id = ''){
+		
+		
+	}
+}
