f2677227470c17d64df44776f80fd9057ac9dd40
cocowool <cocowool@gmail.com>
2015-05-10 16:23:43 +0800

    [前台]厨艺学校前台内容调用

--- application/controllers/chef.php
+++ application/controllers/chef.php
@@ -7,7 +7,22 @@ class Chef extends My_Controller {
 	}
 	
 	public function index($id = ''){
+		$data = array();
+		$data = array_merge($data, $this->getPubData());
+
+		$this->load->model('Chef_Model','c');
+		$data['chef_list'] = $this->c->getAll();
 		
+		foreach ($data['chef_list'] as $k=>$v){
+			if($v['id'] == $id or $v['s_url'] == $id){
+				$data['current_chef'] = $v;
+			}
+		}
 		
+		if(!isset($data['current_chef'])){
+			$data['current_chef'] = $data['chef_list'][0];
+		}
+		
+		$this->load->view('chef/detail', $data);
 	}
 }
