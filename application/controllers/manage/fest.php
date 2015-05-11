a6b72d86beaa4b32b413c216c340c6e927c7c3e6
Green Wang <cocowool@gmail.com>
2015-05-11 17:19:02 +0800

    大会控制器

--- application/controllers/manage/fest.php
+++ application/controllers/manage/fest.php
@@ -14,4 +14,19 @@ class Fest extends MY_Controller {
 		$data['content_data'] = $data;
 		$this->load->view('manage/main', $data);
 	}
+	
+	public function serverside(){
+		$request = $this->input->post();
+		$this->load->model('store_model','s');
+		
+		$data = $this->s->dtRequest($request);
+		//可以在此处进行返回数据的自定义处理
+		foreach($data['data'] as $k=>$v){
+			$data['data'][$k]['s_thumb'] = '<a href=""><img src="' .$v['s_thumb'] .'" height="50px" /></a>';
+			$data['data'][$k]['operation'] = '<a href="/manage/store/edit/' . $v['id'] . '">编辑</a>&nbsp;&nbsp;';
+			$data['data'][$k]['operation'] .= '<a href="/manage/store/del/' . $v['id'] . '">删除</a>&nbsp;&nbsp;';
+		}
+		
+		echo json_encode($data);
+	}
 }
