c043b48e0de15f2652ba80a04fc0051ded95a070
cocowool <cocowool@gmail.com>
2015-05-11 22:20:49 +0800

    [前台]大会详情页

--- application/controllers/fest.php
+++ application/controllers/fest.php
@@ -6,7 +6,26 @@ class Fest extends My_Controller {
 		parent::__construct();
 	}
 	
-	public function index(){
+	public function index($year = ''){
+		$data = array();
+		$data = array_merge($data, $this->getPubData());
+		$this->load->model('Fest_Model', 'f');
+		$this->load->model('Article_Model', 'a');
+		$this->load->model('Category_Model', 'c');
+		$this->load->model('Resource_Model', 'r');
 		
+		$option = array();
+		$data['fest_total'] = $this->f->getTotal($option);
+		$data['fest_list'] = $this->f->getAll($option, $year, 9);
+		
+		$this->load->library('pagination');
+		$config['base_url'] = '/fest/index/';
+		$config['total_rows'] = $data['fest_total'];
+		$config['uri_segment'] = 4;
+		$config['per_page'] = 9;
+		$this->pagination->initialize($config);
+		$data['page_links'] = $this->pagination->create_links();
+		
+		$this->load->view('fest/detail', $data);
 	}
 }
--- application/views/fest/detail.php
+++ application/views/fest/detail.php
@@ -1,1 +1,205 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<title></title>
+	<meta name="keyword" content="">
+	<meta name="description" content="">
+	<link rel="icon" href="/favicon.ico">
+	<link href="/libs/bootstrap-3.3.4/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="/libs/bootstrap-3.3.4/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+	<link href="/templates/yueshi/css/main.css" rel="stylesheet" media="screen">
+	<!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+	<?php $this->load->view('common/header'); ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10">
+				<div class='ys_top_title'>
+					<h1><a href="javascript:void(0);">悦食中国</a></h1>
+				</div>
+				
+				<div class="ys_breadcrum">
+					<p>
+						<?php //echo $breadcrum; ?>
+					</p>
+				</div>
+      			
+      			<div class="ys_video">
+      				<h1><?php echo $s_title; ?></h1>
+      				<h2><?php //echo $subtitle; ?></h2>
+      				<div class="ys_store_desc">
+      					<?php echo $s_desc; ?>
+      				</div>
+      				<div class="ys_article_stat">
+      					<p>
+      						<span>阅读：</span><?php echo $s_click; ?>&nbsp;&nbsp;
+      						<button type="button" class="btn btn-default btn-xs ys_like">
+								<span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 喜欢 <em><?php echo $s_like; ?></em>
+							</button>
+      						<button type="button" class="btn btn-default btn-xs ys_fav">
+								<span class="glyphicon glyphicon-star" aria-hidden="true"></span> 收藏 <em><?php echo $s_fav; ?></em>
+							</button>
+      					</p>
+      				</div>
+      				<div class="share-links">
+						<!-- JiaThis Button BEGIN -->
+						<div class="jiathis_style">
+						<a class="jiathis_button_tsina"></a>
+						<a class="jiathis_button_weixin"></a>
+						<a class="jiathis_button_email"></a>
+						<a class="jiathis_button_douban"></a>
+						<a href="http://www.jiathis.com/share?uid=905086" class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" target="_blank"></a>
+						<a class="jiathis_counter_style"></a>
+						</div>
+						<script type="text/javascript" >
+						var jiathis_config={
+							data_track_clickback:true,
+							summary:"",
+							hideMore:false
+						}
+						</script>
+						<script type="text/javascript" src="http://v3.jiathis.com/code/jia.js?uid=905086" charset="utf-8"></script>
+						<!-- JiaThis Button END -->						
+					</div>
+      				<div class="ys_article_comment">
+      					<div class="ys_comment_recorder">
+      						<?php 
+      						if( isset($sess_data['gUsername']) and !empty($sess_data['gUsername']) ){
+							?>
+							<form name='commentForm' id='commentForm' method='post' action='/comment'>
+								<div class="form-group">
+									<label for="comment">评论内容：</label>
+									<textarea rows="3" cols="" class="form-control" id="content" name="content"></textarea>
+								</div>
+								<div class="form-inline">
+									<div class="form-group">
+										<label form="verifycode">验证码：</label>
+										<input type="text" id="verifycode" name="verifycode" class="form-control input-sm">
+										<img src="http://www.yueshichina.com/register/captcha" />
+									</div>
+								</div>
+								<p>&nbsp;
+									<input type="hidden" id="ctype" name="ctype" value="1">
+									<input type="hidden" id="cid" name="cid" value="<?php echo $id; ?>">
+									<input type="hidden" id="userid" name="userid" value="<?php echo $sess_data['gUserid']; ?>">
+								</p>
+								<div class="form-group">
+									<input type="button" id="btnComment" name="btnComment" value="提交" class="btn btn-primary">
+									<span class="ys_ajax_msg"></span>
+								</div>
+							</form>
+							<?php
+							}else{
+							?>
+							<div>
+								<p>发表留言，请先<a href="/user/login" class="ys_link_red">登录</a></p>
+							</div>
+							<?php
+							}
+      						?>
+      					</div>
+      					
+      					<div class="ys_comment_list">
+      							<?php 
+      							if(isset($comment_list) and is_array($comment_list)){
+      							foreach ($comment_list as $k=>$v){
+								?>
+      						<div class="ys_comment_item container-fluid">
+      						
+      							<div class="row">
+      								<div class="col-md-2">
+      								<?php
+      									if(empty($v['userinfo']['usericon'])){
+      										echo "<img src='" . $this->config->item('default_icon') . "' width='50px' />";
+      									}else{
+											echo "<img src='" . $v['userinfo']['usericon'] . "' width='50px'>";
+										}
+      								?>
+      								</div>
+      								<div class="col-md-10">
+      									<p><?php echo $v['userinfo']['username']; ?>&nbsp;&nbsp;<?php echo $v['insert_time']; ?></p>
+      									<p><?php echo $v['content']; ?></p>
+      								</div>
+      							</div>
+      						</div>
+								<?php
+								}
+								}
+      							?>
+      					</div>
+      				</div>
+      				<div class="ys_article_related">
+      					<h3>相关影像</h3>
+      					<div class="ys_container">
+      						<div class="row">
+      						<?php
+      						if($related_store){
+      						$count = 0;
+      						$html = '';
+      						foreach ($related_store as $k=>$v){
+								$html .= '<div class="col-md-4"><div class="ys_thumbnail_block">';
+								$html .= '<a href="/store/detail/'.$v['id'].'"><img src="' . $v['s_thumb'] . '" /></a>';
+								$html .= '<div class="ys_caption"><h3><a href="/store/detail/' . $v['id'] . '">' . $v['s_title'] . '</a></h3></div></div></div>';
+								$count++;
+								if($count>2) break;
+							}
+							
+							echo $html;
+							}
+      						?>
+      						</div>
+      					</div>
+      				</div>
+      			</div>
+			</div>
+			<div class="col-md-2">
+				<div class="ys_logo">
+					<h2><a href="javascript:void(0);">悦食中国</a></h2>
+					<div class="ys_date">
+						<p><?php echo date('Y', time()); ?></p>
+						<p><?php echo date('m', time()); ?></p>
+						<p><?php echo date('d', time()); ?></p>
+					</div>
+				</div>
+				<div id="YS_MENU" class="ys_tie_<?php echo $tie; ?>">
+					<?php $this->load->view('common/menu'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php $this->load->view('common/footer'); ?>
+	<script type="text/javascript" src="/libs/jquery/jquery-1.11.2.min.js"></script>
+	<script type="text/javascript" src="/libs/bootstrap-3.3.4/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/templates/yueshi/js/main.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+		$('.ys_like').click(function(){
+			$.ajax({
+				dataType : 'json',
+				url	:	'/store/feedback/like/<?php echo $id; ?>',
+				success : function(result){
+					$('.ys_like em').html(result.count);
+				}
+			});
+		});
+		$('.ys_fav').click(function(){
+			$.ajax({
+				dataType : 'json',
+				url	:	'/store/feedback/fav/<?php echo $id; ?>',
+				success : function(result){
+					$('.ys_fav em').html(result.count);
+				}
+			});
+		});
+	});
+	</script>
+</body>
+</html>
