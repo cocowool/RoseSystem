f1fb9e933789a72abe31ec04212fd59c41a22c60
cocowool <cocowool@gmail.com>
2015-05-11 20:31:24 +0800

    [后台]后台顶部菜单

--- application/views/manage/main.php
+++ application/views/manage/main.php
@@ -35,14 +35,8 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
+            <li><a href="#">退出</a></li>
           </ul>
-          <form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
-          </form>
         </div>
       </div>
     </nav>
