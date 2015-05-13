9da17a26148d6b4f861ed11f81e9f5691c472873
cocowool <cocowool@gmail.com>
2015-05-13 22:19:30 +0800

    [后台]完成大会顾问添加

--- application/controllers/manage/fest.php
+++ application/controllers/manage/fest.php
@@ -36,13 +36,13 @@ class Fest extends MY_Controller {
 		$this->lang->load('form_validation', 'chinese');
 		$validations = array(
 				array(
-						'field'	=>	'f_year',
-						'label'	=>	'大会年份',
+						'field'	=>	'f_name',
+						'label'	=>	'顾问姓名',
 						'rules'	=>	'trim|required'
 				),
 				array(
-						'field'	=>	'f_name',
-						'label'	=>	'大会名称',
+						'field'	=>	'f_words',
+						'label'	=>	'顾问评价',
 						'rules'	=>	'trim|required'
 				),
 				array(
@@ -56,7 +56,7 @@ class Fest extends MY_Controller {
 		$this->form_validation->set_error_delimiters('<span class="help-block">', '</span>');
 		
 		if($this->form_validation->run() == FALSE){
-			$data['html_form'] = $this->generate_add_form($this->s, 'manage/fest/add');
+			$data['html_form'] = $this->generate_add_form($this->s, 'manage/fest/consultant/add');
 			$data['content_view'] = 'manage/fest/fest_add';
 			$data['content_data'] = '';
 			$this->load->view('manage/main', $data);
