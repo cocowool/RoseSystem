7ffd9e9b85f5d975b42155bfe409acfa2d55d80f
cocowool <cocowool@gmail.com>
2015-05-14 22:04:59 +0800

    [后台]嘉年华添加页面

--- application/controllers/manage/fest.php
+++ application/controllers/manage/fest.php
@@ -41,6 +41,46 @@ class Fest extends MY_Controller {
 		$this->load->view('manage/main', $data);
 	}
 	
+	private function carnival_add(){
+		$data = array();
+		$this->load->model('Forum_Model','s');
+		$this->lang->load('form_validation', 'chinese');
+		$validations = array(
+				array(
+						'field'	=>	'f_title',
+						'label'	=>	'自定义标题',
+						'rules'	=>	'trim|required'
+				),
+				array(
+						'field'	=>	'f_order',
+						'label'	=>	'排序',
+						'rules'	=>	'trim|required'
+				),
+				array(
+						'field'	=>	'f_thumb',
+						'label'	=>	'缩略图',
+						'rules'	=>	'trim|required'
+				)
+		);
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules($validations);
+		$this->form_validation->set_error_delimiters('<span class="help-block">', '</span>');
+		
+		if($this->form_validation->run() == FALSE){
+			$data['html_form'] = $this->generate_add_form($this->s, 'manage/fest/forum/add');
+			$data['content_view'] = 'manage/fest/forum_add';
+			$data['content_data'] = '';
+			$this->load->view('manage/main', $data);
+		}else{
+			$this->load->helper('date');
+			$_POST['insert_time'] = unix_to_human( local_to_gmt(), TRUE, 'eu');
+			$data = $this->input->post(NULL, true);
+		
+			$result = $this->s->insert( $data );
+			$this->redirectAction($result, $data, '/manage/fest/forum', '/manage/fest/forum/add');
+		}		
+	}
+	
 	public function forum($action = 'list', $id = ''){
 		switch ($action){
 			case 'add':
