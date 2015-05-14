9f750086ed048b72acca6766b84b94a918982700
Green Wang <cocowool@gmail.com>
2015-05-14 09:39:22 +0800

    [后台]完善悦食论坛数据模型

--- application/models/forum_model.php
+++ application/models/forum_model.php
@@ -5,12 +5,21 @@ class Forum_Model extends MY_Model {
 	public $id	=	'id';
 	public $fields = array(
 			array('name'=>'id', 'comment'=>'序号', 'primary' => TRUE, 'form'=>array('type'=>'primary')),
+			array('name'=>'fid', 'comment'=>'大会ID', 'form'=> array('type'=>'select', 'option'=>array('type'=>'function', 'data'=>array(
+					'model'=>'fest_model',
+					'name'=>'get_all_fest',
+					'parameter'=> array('0'),
+			)))),
 			array('name'=>'aid', 'comment'=>'杂志ID', 'form'=> array('type'=>'select', 'option'=>array('type'=>'function', 'data'=>array(
 					'model'=>'article_model',
 					'name'=>'getById',
 					'parameter'=> array('0'),
 			)))),
+			array('name'=>'f_type', 'comment'=>'资源类型', 'form'=>array('type'=>'select', 
+				'option'=>array('type'=>'static', 'data'=>array('1'=>'杂志','2'=>'影像')))
+			),
 			array('name'=>'f_title', 'comment'=>'自定标题', 'form'=> array('type'=>'text', 'validation'=>'required', 'tips'=>'请输入顾问姓名')),
+			array('name'=>'f_thumb', 'comment'=>'缩略图', 'form'=> array('type'=>'text', 'validation'=>'required', 'tips'=>'请输入顾问姓名')),
 			array('name'=>'f_order', 'comment'=>'排序', 'form'=> array('type'=>'text')),
 			array('name'=>'status', 'comment'=>'状态', 'form'=>array('type'=>'select', 
 				'option'=>array('type'=>'static', 'data'=>array('0'=>'草稿','1'=>'已发布')))
