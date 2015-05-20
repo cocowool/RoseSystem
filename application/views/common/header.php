9f2c9b1613da3474730a0f65dab043ba09d6535e
cocowool <cocowool@gmail.com>
2015-05-20 20:51:48 +0800

    [前台]调整INPUT尺寸

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -15,10 +15,10 @@
 					<form name='ys_login' id='ys_login' method='post' class='navbar-form navbar-right' action='/user/login'>
 						<a href="/user/register">注册</a>
 						<div class="form-group">
-							<input type='text' name='username' id='username' placeholder="请输入用户名" class="form-control" >
+							<input type='text' name='username' id='username' placeholder="请输入用户名" class="form-control input-sm" >
 						</div>
 						<div class="form-group">
-							<input type='password' name='password' id='password' placeholder="请输入密码" class="form-control" >
+							<input type='password' name='password' id='password' placeholder="请输入密码" class="form-control input-sm" >
 						<input type='hidden' name='referer' id='referer' value='<?php echo current_url(); ?>' />
 						</div>
 						<a href="javascript:void(0);" id='loginBtn'>登录</a>
