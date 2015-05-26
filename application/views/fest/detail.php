fbcda5feadab0e05339116822b78f7f8b801d162
Green Wang <cocowool@gmail.com>
2015-05-26 12:46:25 +0800

    [前台]嘉年华前台页面

--- application/views/fest/detail.php
+++ application/views/fest/detail.php
@@ -125,44 +125,29 @@
 							foreach ($carnival_list as $k=>$v){
 								$style = '';
 								if($k==0) $style = 'active';
-								$carnival_html .= '<li data-target="#carousel-example-generic" data-slide-to="'.'" class="active"></li>';
+								$carnival_html .= '<li data-target="#carousel-example-generic" data-slide-to="'.$k.'" class="'.$style.'"></li>';
 							}
 							$carnival_html .= '</ol>';
-      					?>      						
+							$carnival_html .= '<div class="carousel-inner" role="listbox">';
+							foreach ($carnival_list as $k=>$v){
+								$style = '';
+								if($k==0) $style = 'active';
+								$carnival_html .= '<div class="item '.$style.'">';
+								$carnival_html .= '<img src="'.$v['f_thumb'].'" alt="">';
+								$carnival_html .= '<div class="carousel-caption"></div>';
+								$carnival_html .= '</div>';
+							}
+							$carnival_html .= '</div>';
+							$carnival_html .= '<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">';
+							$carnival_html .= '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>';
+							$carnival_html .= '<span class="sr-only">Previous</span></a>';
+							$carnival_html .= '<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">';
+							$carnival_html .= '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>';
+							$carnival_html .= '<span class="sr-only">Next</span></a>';
+							$carnival_html .= '</div></div>';
       					
-      						
-								<!-- Indicators -->
-								
-									
-									<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-								
-								<!-- Wrapper for slides -->
-								<div class="carousel-inner" role="listbox">
-									<div class="item active">
-										<img src="/temp/c1.jpg" alt="">
-										<div class="carousel-caption">
-								        XXX
-										</div>
-									</div>
-									<div class="item">
-										<img src="/temp/c2.jpg" alt="">
-										<div class="carousel-caption">
-								        YYY
-										</div>
-									</div>
-								</div>
-								<!-- Controls -->
-								<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-									<span class="sr-only">Previous</span>
-								</a>
-								<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-									<span class="sr-only">Next</span>
-								</a>
-							</div>
-      					</div>
-      					<?php } ?>
+							echo $carnival_html;
+      					} ?>
       					<div class="ys_go_top">
       						<p><a href="#top">回到顶部</a></p>
       					</div>
