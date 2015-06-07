5547b2daa6395af95db0c34d72260761a20577e6
cocowool <cocowool@gmail.com>
2015-06-07 21:28:05 +0800

    [后台]完成后台编辑

--- application/views/manage/resource/resource_list.php
+++ application/views/manage/resource/resource_list.php
@@ -30,6 +30,8 @@
 </div>
 <script type="text/javascript">
 $(document).ready(function(){
+	
+	
 	var dt = $('#rs_table').DataTable({
 		'processing': true,
 		'serverSide': true,
@@ -64,6 +66,51 @@ $(document).ready(function(){
 				"defaultContent":""
 			}
 		]		
+	});
+
+
+	//点击文本框复制其内容到剪贴板上方法
+	function copyToClipboard(txt) {
+	    if (window.clipboardData) {
+	        window.clipboardData.clearData();
+	        window.clipboardData.setData("Text", txt);
+	        alert("已经成功复制到剪帖板上！");
+	    } else if (navigator.userAgent.indexOf("Opera") != -1) {
+	        window.location = txt;
+	    } else if (window.netscape) {
+	        try {
+	            netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+	        } catch (e) {
+	            alert("被浏览器拒绝！\n请在浏览器地址栏输入'about:config'并回车\n然后将'signed.applets.codebase_principal_support'设置为'true'");
+	        }
+	        var clip = Components.classes['@mozilla.org/widget/clipboard;1'].createInstance(Components.interfaces.nsIClipboard);
+	        if (!clip) return;
+	        var trans = Components.classes['@mozilla.org/widget/transferable;1'].createInstance(Components.interfaces.nsITransferable);
+	        if (!trans) return;
+	        trans.addDataFlavor('text/unicode');
+	        var str = new Object();
+	        var len = new Object();
+	        var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+	        var copytext = txt;
+	        str.data = copytext;
+	        trans.setTransferData("text/unicode", str, copytext.length * 2);
+	        var clipid = Components.interfaces.nsIClipboard;
+	        if (!clip) return false;
+	        clip.setData(trans, null, clipid.kGlobalClipboard);
+	        alert("已经成功复制到剪帖板上！");
+	    }
+	}
+
+// 	$("#rs_table .ys_copylink").live('click', function(){
+// 		//alert('TEST');
+// 		console.log('TEST');
+// 	});
+
+	$('[data-toggle="popover"]').popover();
+	
+	$('#rs_table').delegate('.ys_copylink', 'click', function(e){
+		e.preventDefault();
+		copyToClipboard($(this).attr('href'));
 	});
 });
 </script>
